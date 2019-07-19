13d37b86593d9f1a851e324d2043e28ec17f6e6d
Idriss Mahjoubi <mahjoubi.idris@gmail.com>
2019-07-19 10:45:30 +0100

    yarn -> npm front script

--- routes/event.route.js
+++ routes/event.route.js
@@ -3,49 +3,11 @@ const eventModel = require('../models/event.model');
 const passport = require('passport');
 const upload = require('../utils/Uploader');
 
-// const multer = require('multer');
-
-// const storage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     cb(null, './uploads');
-//   },
-//   filename(req, file, cb) {
-//     cb(null, new Date().toISOString().replace(/:/g, '-') + file.originalname);
-//   }
-// });
-
-// const upload = multer({ storage });
-
-/*****************  All routes require authorization *****************/
-
-// router.get('/search', (req, res) => {
-//   const title = req.query.title;
-//   eventModel
-//     .find({ title: new RegExp(title, 'i') })
-//     .sort('-date')
-//     .then(data => {
-//       res.json(data);
-//     })
-//     .catch(err => res.status(400).json(err));
-// });
-
-// router.get('/filter', (req, res) => {
-//   const type = req.query.type;
-//   eventModel
-//     .find({ type })
-//     .sort('-date')
-//     .then(data => {
-//       res.json(data);
-//     })
-//     .catch(err => res.status(400).json(err));
-// });
 
 /* GET All Events . 
 @Route : events/
 */
 router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
-  // const archive = req.query.isArchived;
-
   eventModel
     .find()
     .sort('-date')
@@ -54,15 +16,6 @@ router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => 
     })
     .catch(err => res.send(err));
 
-  // else {
-  //   eventModel
-  //     .find({ archive })
-  //     .sort('-date')
-  //     .then(data => {
-  //       res.json(data);
-  //     })
-  //     .catch(err => res.send(err));
-  // }
 });
 
 /* GET Single Events . 
@@ -103,7 +56,6 @@ router.post(
       user: req.body.user
     });
 
-    console.log('newEvent nodejs:', newEvent);
     newEvent
       .save()
       .then(event => res.json(event))
@@ -203,62 +155,6 @@ router.put('/unarchive/:id', (req, res) => {
   .catch(err => res.status(400).json(err));
 });
 
-// router.get('/id/:id', function(req, res) {
-//   var now = new Date();
-//   let query = {
-//     _id: req.params.id
-//   };
-//   console.log('id' + req.params.id);
-//   event.findById(
-//     req.params.id,
 
-//     function(err, meetings) {
-//       if (err) return res.send(err);
-//       res.send(meetings);
-//     }
-//   );
-// });
-
-// router.get('/sportif', function(req, res, next) {
-//   var users = null;
-//   var now = new Date();
-//   event
-//     .find()
-//     .sort('-date')
-//     .then(data => {
-//       // res.setHeader("Access-Control-Allow-Origin", "*"),
-//       // res.statusCode=200,
-//       //res.contentType('application/json'),
-//       // res.json(data)
-//       let sports = [];
-//       data.forEach(element => {
-//         if (element.type === 'sportif') {
-//           sports.push(element);
-//         }
-//       });
-//       res.json(sports);
-//     });
-// });
-
-// router.get('/autres', function(req, res, next) {
-//   var users = null;
-//   var now = new Date();
-//   event
-//     .find()
-//     .sort('-date')
-//     .then(data => {
-//       // res.setHeader("Access-Control-Allow-Origin", "*"),
-//       // res.statusCode=200,
-//       //res.contentType('application/json'),
-//       // res.json(data)
-//       let sports = [];
-//       data.forEach(element => {
-//         if (element.type === 'autres') {
-//           sports.push(element);
-//         }
-//       });
-//       res.json(sports);
-//     });
-// });
 
 module.exports = router;
